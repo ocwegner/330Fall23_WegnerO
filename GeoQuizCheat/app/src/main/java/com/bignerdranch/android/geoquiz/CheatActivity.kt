c9efe69f3e16c18c242f5ec664c625978977aee7
ocwegner <125328570+ocwegner@users.noreply.github.com>
2023-09-29 14:36:27 -0500

    Geo Quiz Cheat

--- GeoQuizCheat/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
+++ GeoQuizCheat/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -1,17 +1,19 @@
 package com.bignerdranch.android.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bignerdranch.android.geoquiz.databinding.ActivityCheatBinding
 
 const val EXTRA_ANSWER_KEY = "com.bignerdranch.android.geoquiz.answer"
+const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
 class CheatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCheatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCheatBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val answer = this.intent.getBooleanExtra(EXTRA_ANSWER_KEY, false)
         binding.showAnswerButton.setOnClickListener{
             val textVal = when {
@@ -19,6 +21,10 @@ class CheatActivity : AppCompatActivity() {
                 else -> R.string.false_button
             }
             binding.answerTextView.setText(textVal)
+            val intentData = Intent().apply {
+                putExtra(EXTRA_ANSWER_SHOWN, true)
+            }
+            setResult(Activity.RESULT_OK, intentData)
         }
     }
 }
