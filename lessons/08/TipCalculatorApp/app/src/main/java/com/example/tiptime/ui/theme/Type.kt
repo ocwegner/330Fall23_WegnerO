3c79f80917cb4319df77299d4de3bdb7a6f9217c
ocwegner <125328570+ocwegner@users.noreply.github.com>
2023-10-29 16:41:25 -0500

    Automated Tests 10/29/23

---
+++ lessons/08/TipCalculatorApp/app/src/main/java/com/example/tiptime/ui/theme/Type.kt
@@ -0,0 +1,18 @@
+package com.example.tiptime.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    displaySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp,
+    )
+)
