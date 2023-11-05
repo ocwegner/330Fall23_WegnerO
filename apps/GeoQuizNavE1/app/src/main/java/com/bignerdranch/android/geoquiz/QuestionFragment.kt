41740b066935410a59fbcd5b3c3457c53c7b72da
ocwegner <125328570+ocwegner@users.noreply.github.com>
2023-11-05 17:48:00 -0600

    GeoQuizE1 Final

--- apps/GeoQuizNavE1/app/src/main/java/com/bignerdranch/android/geoquiz/QuestionFragment.kt
+++ apps/GeoQuizNavE1/app/src/main/java/com/bignerdranch/android/geoquiz/QuestionFragment.kt
@@ -17,6 +17,7 @@ private const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
 class QuestionFragment : Fragment() {
     private var _binding: FragmentQuestionBinding? = null
     private val binding get() = _binding!!
+    private var userScore = 0
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -25,6 +26,19 @@ class QuestionFragment : Fragment() {
         Question(R.string.question_africa, false),
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true),
+        Question(R.string.question_amazon, true),
+        Question(R.string.question_uk, false),
+        Question(R.string.question_antarctica, true),
+        Question(R.string.question_great_barrier_reef, false),
+        Question(R.string.question_everest, true),
+        Question(R.string.question_chile, false),
+        Question(R.string.question_russia, true),
+        Question(R.string.question_united_states, true),
+        Question(R.string.question_pacific, true),
+        Question(R.string.question_dead_sea, true),
+        Question(R.string.question_france, false),
+        Question(R.string.question_prime_meridian, true),
+        Question(R.string.question_andes, true)
     )
     private var currentIndex = 0
     private var isCheater = false
@@ -42,23 +56,73 @@ class QuestionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        //disable true+false buttons after initial answer
+        fun isAnswered(currentIndex: Int) {
+            val isQuestionAnswered = questionBank[currentIndex].answered
+            binding.trueButton.isEnabled = !isQuestionAnswered
+            binding.falseButton.isEnabled = !isQuestionAnswered
+        }
+
         if (savedInstanceState != null) {
             Log.d(TAG, "savedInstanceState is set.")
             currentIndex = savedInstanceState.getInt(CURRENT_INDEX_KEY, 0)
         }
         binding.questionText.setText(questionBank[currentIndex].testResId)
 
-        binding.nextButton.setOnClickListener {
+        //make a PREV button
+        binding.previousButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
+            if (currentIndex >= 0) {
+                isAnswered(currentIndex)
+                binding.questionText.setText(questionBank[currentIndex].testResId)
+            }
+            else {
+                currentIndex = 0
+                Toast.makeText(this.context, "Can't go back farther!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.questionText.setOnClickListener{
             currentIndex = (currentIndex + 1) % questionBank.size
             binding.questionText.setText(questionBank[currentIndex].testResId)
         }
 
         binding.trueButton.setOnClickListener {
+            binding.trueButton.isEnabled = false
+            binding.falseButton.isEnabled = false
+            questionBank[currentIndex].answered = true
             checkAnswer(true)
+            if (currentIndex == questionBank.size - 1){
+                fun displayScore() {
+                    val finalScoreNum = (userScore.toDouble() / questionBank.size.toDouble()) * 100
+                    val finalScore = finalScoreNum.toString().format("%2f") + "%"
+                    val stringForToast = userScore.toString().plus(" out of ").plus(questionBank.size.toString()).plus(" correct. ").plus(finalScore)
+                    Toast.makeText(this.context, stringForToast, Toast.LENGTH_SHORT).show()
+                }
+                displayScore()
+            }
         }
 
         binding.falseButton.setOnClickListener {
+            binding.trueButton.isEnabled = false
+            binding.falseButton.isEnabled = false
+            questionBank[currentIndex].answered = true
             checkAnswer(false)
+            if (currentIndex == questionBank.size - 1){
+                fun displayScore() {
+                    val finalScoreNum = (userScore.toDouble() / questionBank.size.toDouble()) * 100
+                    val finalScore = "%.2f".format(finalScoreNum.toString()) + "%"
+                    val stringForToast = userScore.toString().plus(" out of ").plus(questionBank.size.toString()).plus(" correct. ").plus(finalScore)
+                    Toast.makeText(this.context, stringForToast, Toast.LENGTH_SHORT).show()
+                }
+                displayScore()
+            }
+        }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            isAnswered(currentIndex)
+            binding.questionText.setText(questionBank[currentIndex].testResId)
         }
 
         binding.cheatButton.setOnClickListener {
@@ -67,6 +131,13 @@ class QuestionFragment : Fragment() {
             val action = QuestionFragmentDirections.actionQuestionFragmentToCheatFragment2(answer)
             this.findNavController().navigate(action)
         }
+    }
+
+    private fun displayScore() {
+        val finalScoreNum = (userScore.toDouble() / questionBank.size.toDouble()) * 100
+        val finalScore = finalScoreNum.toString().format("%2f") + "%"
+        val stringForToast = userScore.toString().plus(" out of ").plus(questionBank.size.toString()).plus(" correct. ").plus(finalScore)
+        Toast.makeText(this.context, stringForToast, Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
@@ -89,6 +160,10 @@ class QuestionFragment : Fragment() {
             else -> R.string.incorrect_toast
         }
         isCheater = false
+
+        if (correctAnswer == userAnswer){
+            userScore += 1
+        }
 
         Toast.makeText(this.context, resId, Toast.LENGTH_SHORT).show()
     }
