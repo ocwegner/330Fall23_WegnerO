d1211003c5a8ebfd49e0068f35900ba9ee03e394
ocwegner <125328570+ocwegner@users.noreply.github.com>
2023-11-15 13:23:30 -0600

    Lesson 10

--- lessons/10/android-basics-kotlin-bus-schedule-app-main/app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.busschedule
-
-import android.app.Application
-import com.example.busschedule.database.AppDatabase
-
-class BusScheduleApplication : Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
-}
